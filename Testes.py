8b355673ce62601b53c5fab25749eeb5270871e8
Lucas P. Nunes <lucaspereiranunes63@gmail.com>
2026-09-02 13:16:53 -0300

    test: Arquivo de testes refatorado e adicionado novos testes

--- Testes.py
+++ Testes.py
@@ -1,16 +1,63 @@
-from criptolib import (
-    AritmeticaModular
-)
+from criptolib import AritmeticaModular
+
 
 print("=== ARITMÉTICA MODULAR ===")
 
-print("Adição: 17 + 8 mod 5 =", AritmeticaModular.adicao(17, 8, 5))
-print("Subtração: 17 - 8 mod 5 =", AritmeticaModular.subtracao(17, 8, 5))
-print("Multiplicação: 17 * 8 mod 5 =" , AritmeticaModular.multiplicacao(17, 8, 5))
-print("Potenciação: 17^8 mod 5 =", AritmeticaModular.exponenciacao(11, 7, 13))
+a = 17
+b = 8
+n = 5
 
-print("\n=== MDC ===")
+print(f"Adição: {a} + {b} mod {n} =", 
+      AritmeticaModular.adicao(a, b, n))
 
-print("MDC(60, 24):", AritmeticaModular.mdc(60, 24))
-print("MDC(-60, 24):", AritmeticaModular.mdc(-60, 24))
-print("MDC(25, 0):", AritmeticaModular.mdc(25, 0))
+print(f"Subtração: {a} - {b} mod {n} =", 
+      AritmeticaModular.subtracao(a, b, n))
+
+print(f"Multiplicação: {a} * {b} mod {n} =", 
+      AritmeticaModular.multiplicacao(a, b, n))
+
+print(f"Potenciação: {a}^{b} mod {n} =", 
+      AritmeticaModular.exponenciacao(a, b, n))
+
+
+print("\n=== MDC POR TENTATIVA ===")
+
+print("MDC(60, 24):", 
+      AritmeticaModular.mdc(60, 24))
+
+print("MDC(-60, 24):", 
+      AritmeticaModular.mdc(-60, 24))
+
+print("MDC(25, 0):", 
+      AritmeticaModular.mdc(25, 0))
+
+
+print("\n=== ALGORITMO DE EUCLIDES ===")
+
+print("Euclides(60, 24):", 
+      AritmeticaModular.euclides(60, 24))
+
+print("Euclides(-60, 24):", 
+      AritmeticaModular.euclides(-60, 24))
+
+print("Euclides(25, 0):", 
+      AritmeticaModular.euclides(25, 0))
+
+
+print("\n=== EUCLIDES ESTENDIDO ===")
+
+mdc, x, y = AritmeticaModular.euclides_estendido(60, 24)
+
+print("Entrada: a = 60, b = 24")
+print("MDC:", mdc)
+print("x:", x)
+print("y:", y)
+print(f"Verificação: 60 * ({x}) + 24 * ({y}) =", 60 * x + 24 * y)
+
+mdc, x, y = AritmeticaModular.euclides_estendido(-60, 24)
+
+print("\nEntrada: a = -60, b = 24")
+print("MDC:", mdc)
+print("x:", x)
+print("y:", y)
+print(f"Verificação: (-60) * ({x}) + 24 * ({y}) =", -60 * x + 24 * y)
